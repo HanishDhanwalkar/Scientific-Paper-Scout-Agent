9cf565b25d827a3e75c3618980791f70b9fd9dfd
HanishDhanwalkar <hanishpd1729@gmail.com>
2025-06-28 23:00:11 +0530

    llm

--- llm_handler.py
+++ llm_handler.py
@@ -1,12 +1,18 @@
 import json
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage, ToolMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.tools import tool
 
 from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+# Import configuration
 from config import (
     CURRENT_LLM_PROVIDER,
+    CURRENT_SUMMARIZER_LLM_PROVIDER,
     OLLAMA_MODEL,
     OPENAI_MODEL,
     CLAUDE_MODEL,
@@ -17,44 +23,51 @@ from config import (
     OLLAMA_BASE_URL
 )
 
-def convert_to_llm_tool(tool):
-    tool_schema = {
-        "type": "function",
-        "function": {
-            "name": tool.name,
-            "description": tool.description,
-            "type": "function",
-            "parameters": {
-                "type": "object",
-                "properties": tool.inputSchema["properties"]
-            }
-        }
-    }
-
-    return tool_schema
-
 def get_llm_client():
     """Initializes and returns the appropriate LLM client based on configuration."""
     if CURRENT_LLM_PROVIDER == "ollama":
-        print(f"Using Ollama model: {OLLAMA_MODEL}")
+        # print(f"Using Ollama model: {OLLAMA_MODEL}")
         return ChatOllama(model=OLLAMA_MODEL, base_url=OLLAMA_BASE_URL)
     elif CURRENT_LLM_PROVIDER == "openai":
         if not OPENAI_API_KEY:
             raise ValueError("OPENAI_API_KEY not set in environment variables.")
-        print(f"Using OpenAI model: {OPENAI_MODEL}")
+        # print(f"Using OpenAI model: {OPENAI_MODEL}")
         return ChatOpenAI(model=OPENAI_MODEL, api_key=OPENAI_API_KEY)
     elif CURRENT_LLM_PROVIDER == "claude":
         if not ANTHROPIC_API_KEY:
             raise ValueError("ANTHROPIC_API_KEY not set in environment variables.")
-        print(f"Using Claude model: {CLAUDE_MODEL}")
+        # print(f"Using Claude model: {CLAUDE_MODEL}")
         return ChatAnthropic(model=CLAUDE_MODEL, anthropic_api_key=ANTHROPIC_API_KEY)
     elif CURRENT_LLM_PROVIDER == "gemini":
         if not GOOGLE_API_KEY:
             raise ValueError("GOOGLE_API_KEY not set in environment variables.")
-        print(f"Using Gemini model: {GEMINI_MODEL}")
+        # print(f"Using Gemini model: {GEMINI_MODEL}")
         return ChatGoogleGenerativeAI(model=GEMINI_MODEL, google_api_key=GOOGLE_API_KEY)
     else:
         raise ValueError(f"Unsupported LLM provider: {CURRENT_LLM_PROVIDER}")
+    
+def get_summarizer_llm_client():
+    """Initializes and returns the appropriate LLM client based on configuration."""
+    if CURRENT_SUMMARIZER_LLM_PROVIDER == "ollama":
+        # print(f"Using Ollama model: {OLLAMA_MODEL} as summarizer")
+        return ChatOllama(model=OLLAMA_MODEL, base_url=OLLAMA_BASE_URL)
+    elif CURRENT_SUMMARIZER_LLM_PROVIDER == "openai":
+        if not OPENAI_API_KEY:
+            raise ValueError("OPENAI_API_KEY not set in environment variables.")
+        # print(f"Using OpenAI model: {OPENAI_MODEL} as summarizer")
+        return ChatOpenAI(model=OPENAI_MODEL, api_key=OPENAI_API_KEY)
+    elif CURRENT_SUMMARIZER_LLM_PROVIDER == "claude":
+        if not ANTHROPIC_API_KEY:
+            raise ValueError("ANTHROPIC_API_KEY not set in environment variables.")
+        # print(f"Using Claude model: {CLAUDE_MODEL} as summarizer")
+        return ChatAnthropic(model=CLAUDE_MODEL, anthropic_api_key=ANTHROPIC_API_KEY)
+    elif CURRENT_SUMMARIZER_LLM_PROVIDER == "gemini":
+        if not GOOGLE_API_KEY:
+            raise ValueError("GOOGLE_API_KEY not set in environment variables.")
+        # print(f"Using Gemini model: {GEMINI_MODEL} as summarizer")
+        return ChatGoogleGenerativeAI(model=GEMINI_MODEL, google_api_key=GOOGLE_API_KEY)
+    else:
+        raise ValueError(f"Unsupported LLM provider: {CURRENT_SUMMARIZER_LLM_PROVIDER}")
 
 def call_llm(message_history, functions):
     """
@@ -70,29 +83,21 @@ def call_llm(message_history, functions):
     client = get_llm_client()
     
     print("CALLING LLM")
-    
-    tool_call_present = False
     try:
         response = client.invoke(
             input=message_history,
             tools=functions,
         )
-        
+
         functions_to_call = []
         if response.tool_calls:
             for tool_call in response.tool_calls:
-                name = tool_call['name']
-                args = tool_call['args']
+                name = tool_call.name
+                args = tool_call.args
                 functions_to_call.append({"name": name, "args": args})
-            
-            tool_call_present = True
-                
-        else:
-            print("No tool calls found in response.")
-            return tool_call_present, response.content
 
-        return tool_call_present, functions_to_call
+        return functions_to_call
 
     except Exception as e:
-        # print(f"Error calling LLM: {e}")
-        return tool_call_present, "Error calling LLM: " + str(e)
+        print(f"Error calling LLM: {e}")
+        return []
